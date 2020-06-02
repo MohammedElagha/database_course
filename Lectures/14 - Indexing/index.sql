4ebee0a74421e2db348632bc81a5bc7fe4c90473
Mohammed <dev.magha@gmail.com>
2020-06-02 20:55:02 +0300

    lecture: indexing

---
+++ Lectures/14 - Indexing/index.sql
@@ -0,0 +1,12 @@
+# Indexing
+
+# 4 types: primary, index, unique, fulltext
+
+# index
+CREATE INDEX index_name ON table_name (col_list);
+
+# unique
+CREATE UNIQUE INDEX index_name ON table_name (col_list);
+
+# fulltext
+CREATE FULLTEXT INDEX index_name ON table_name (col_list);
